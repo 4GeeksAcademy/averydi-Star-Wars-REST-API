b9d766d439efa68dfdedcdaccd135d9179094c83
David Avery <avery.di@gmail.com>
2023-05-24 23:58:15 +0000

    Additional Edits in Apps and Models

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User, Planet, Character, Vehicle, Favorite 
+from models import db, User, People, Planets, Starships, Favorites
 #from models import Person
 
 app = Flask(__name__)
@@ -31,162 +31,234 @@ setup_admin(app)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
-#AUTENTICACION
-current_logged_user_id = 1
-
 # generate sitemap with all your endpoints
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def get_users():
+# # Create an API that connects to a database and implements the following Endpoints (very similar to SWAPI.dev or SWAPI.tech):
+
+# # [GET] /people Get a list of all the people in the database
+
+@app.route('/people', methods=['GET'])
+def handle_people():
+
+    people = People.query.all()
+    all_people = list(map(lambda x: x.serialize(), people))
+
+    return jsonify(all_people), 200
+
+# # [GET] /people/<int:people_id> Get a one single people information
+
+@app.route('/people/<int:people_id>', methods=['GET'])
+def handle_person(people_id):
+
+    person = People.query.get(people_id)
+    if person is None:
+        raise APIException("User not found", status_code=404)
+
+    return jsonify(person), 200
+
+# # [GET] /planets Get a list of all the planets in the database
+
+@app.route('/planets', methods=['GET'])
+def handle_planets():
+
+    planets = Planets.query.all()
+    all_planets = list(map(lambda x: x.serialize(), planets))
+
+    return jsonify(all_planets), 200
+
+# # [GET] /planets/<int:planet_id> Get one single planet information
+
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+def handle_planet(planet_id):
+
+    planet = Planets.query.get(planet_id)
+    if planet is None:
+        raise APIException("User not found", status_code=404)
+
+    return jsonify(planet), 200
+
+# # [GET] /starships Get a list of all the starships in the database
+
+@app.route('/starships', methods=['GET'])
+def handle_starships():
+
+    starships = Starships.query.all()
+    all_starships = list(map(lambda x: x.serialize(), starships))
+
+    return jsonify(all_starships), 200
+
+# # [GET] /starships/<int:starship_id> Get one single starship information
+
+@app.route('/starships/<int:starship_id>', methods=['GET'])
+def handle_starship(starship_id):
+
+    starship = Starships.query.get(starship_id)
+    if starship is None:
+        raise APIException("User not found", status_code=404)
+
+    return jsonify(starship), 200
+
+# # Additionally create the following endpoints to allow your StartWars blog to have users and favorites:
+
+# # [GET] /users Get a list of all the blog post users
+
+@app.route('/users', methods=['GET'])
+def handle_users():
 
     users = User.query.all()
+    all_users = list(map(lambda x: x.serialize(), users))
 
-    user_list = [
-        {
-            'id': user.id,
-            'email': user.email
-        }
-        for user in users
-    ]
+    return jsonify(all_users), 200
 
-    return jsonify(user_list), 200
+@app.route('/users', methods=['POST'])
+def create_users():
 
-@app.route('/planet', methods=['GET'])
-def get_planets():
-    allPlanets = Planet.query.all()
-    result = [element.serialize() for element in allPlanets]
-    return jsonify(result), 200
+    request_body_users = request.get_json()
 
-@app.route('/planet', methods=['POST'])
-def post_planet():
-
-    # obtener los datos de la petición que están en formato JSON a un tipo de datos entendibles por pyton (a un diccionario). En principio, en esta petición, deberían enviarnos 3 campos: el nombre, la descripción del planeta y la población
-    data = request.get_json()
-
-    # creamos un nuevo objeto de tipo Planet
-    planet = Planet(name=data['name'], description=data['description'], population=data['population'])
-
-    # añadimos el planeta a la base de datos
-    db.session.add(planet)
+    user1 = User(first_name=request_body_users["first_name"], last_name=request_body_users["last_name"], email=request_body_users["email"], password=request_body_users["password"])
+    db.session.add(user1)
     db.session.commit()
 
-    response_body = {"msg": "Planet inserted successfully"}
-    return jsonify(response_body), 200
+    return jsonify(request_body_users), 200
 
-#CHARACTER
+@app.route('/users/<int:user_id>', methods=['PUT'])
+def update_users(user_id):
 
-@app.route('/character', methods=['GET'])
-def get_characters():
-    allCharacters = Character.query.all()
-    result = [element.serialize() for element in allCharacters]
-    return jsonify(result), 200
+    request_body_users = request.get_json()
 
-@app.route('/character', methods=['POST'])
-def post_character():
-
-    data = request.get_json()
-
-    character = Character(name=data['name'], description=data['description'], eye_color=data['eye_color'])
-
-    db.session.add(character)
+    user1 = User.query.get(user_id)
+    if user1 is None:
+        raise APIException("User not found", status_code=404)
+    if "username" in request_body_users:
+        user1.username = request_body_users["username"]
+    if "first_name" in request_body_users:
+        user1.first_name = request_body_users["first_name"]
+    if "last_name" in request_body_users:
+        user1.first_name = request_body_users["last_name"]
+    if "email" in request_body_users:
+        user1.first_name = request_body_users["email"]
+    
     db.session.commit()
 
-    response_body = {"msg": "Character added succesfully!"}
-    return jsonify(response_body), 200
+    return jsonify(request_body_users), 200
 
+@app.route('/users/<int:user_id>', methods=['DELETE'])
+def delete_user(user_id):
 
-#VEHICLES
-
-@app.route('/vehicle', methods=['GET'])
-def get_vehicles():
-    allVehicles = Vehicle.query.all()
-    result = [element.serialize() for element in allVehicles]
-    return jsonify(result), 200
-
-@app.route('/character', methods=['POST'])
-def post_vehicle():
-
-    data = request.get_json()
-
-    vehicle = Vehicle(name=data['name'], description=data['description'], model=data['model'])
-
-    db.session.add(vehicle)
+    user1 = User.query.get(user_id)
+    if user1 is None:
+        raise APIException("User not found", status_code=404)
+    db.session.delete(user1)
     db.session.commit()
 
-    response_body = {"msg": "Vehicle added succesfully!"}
-    return jsonify(response_body), 200
+    return jsonify("User deletion successful"), 200
 
-#FAVORITE PLANET
+# # [GET] /users/favorites Get all the favorites that belong to the current user.
+
+@app.route('/users/favorites', methods=['GET'])
+def handle_favorites():
+
+    favorites = Favorites.query.all()
+    all_favorites = list(map(lambda x: x.serialize(), favorites))
+
+    return jsonify(all_favorites), 200
+
+# # [POST] /favorite/starship/<int:starship_id> Add a new favorite starship to the current user with the starship id = starship_id.
+
+@app.route('/favorite/starship/<int:starship_id>', methods=['POST'])
+def create_favorite_starship(starship_name):
+
+    request_body_fav_star = request.get_json()
+    starship_name = request_body_fav_star["starship_name"]
+    favorite_id = request_body_fav_star["favorite_id"]
+
+    fav_star = Favorites.query.filter_by(starship_name= starship_name, favorite_id= favorite_id)
+    if fav_star is None:
+        fav_star = Favorites(starship_name= starship_name, favorite_id= favorite_id)
+        db.session.add()
+    else:
+        fav_star = request_body_fav_star[starship_name]
+    db.session.commit()
+
+    return jsonify(fav_star), 200
+
+# # [POST] /favorite/planet/<int:planet_id> Add a new favorite planet to the current user with the planet id = planet_id.
+
 @app.route('/favorite/planet/<int:planet_id>', methods=['POST'])
-def add_favorite_planet(planet_id):
- 
-    user = User.query.get(current_logged_user_id)
+def create_favorite_planet(planet_name):
 
-    new_favorite = Favorite(user_id=current_logged_user_id, planet_id=planet_id)
-    db.session.add(new_favorite)
+    request_body_fav_planet = request.get_json()
+    planet_name = request_body_fav_planet["planet_name"]
+    favorite_id = request_body_fav_planet["favorite_id"]
+
+    fav_planet = Favorites.query.filter_by(planet_name= planet_name, favorite_id= favorite_id)
+    if fav_planet is None:
+        fav_planet = Favorites(planet_name= planet_name, favorite_id= favorite_id)
+        db.session.add()
+    else:
+        fav_planet = request_body_fav_planet[planet_name]
     db.session.commit()
 
-    response_body = {
-        "msg": "Planeta agregado correctamente", 
-        "favorite": new_favorite.serialize()
-    }
+    return jsonify(fav_planet), 200
 
-    return jsonify(response_body), 200
+# # [POST] /favorite/people/<int:people_id> Add new favorite people to the current user with the people id = people_id.
 
-#FAVORITE CHARACTER
-@app.route('/favorite/character/<int:character_id>', methods=['POST'])
-def add_favorite_character(character_id):
- 
-    user = User.query.get(current_logged_user_id)
+@app.route('/favorite/people/<int:people_id>', methods=['POST'])
+def create_favorite_people(people_name):
 
-    new_favorite = Favorite(user_id=current_logged_user_id, character_id = character_id)
-    db.session.add(new_favorite)
+    request_body_fav_person = request.get_json()
+    people_name = request_body_fav_person["people_name"]
+    favorite_id = request_body_fav_person["favorite_id"]
+
+    fav_person = Favorites.query.filter_by(people_name= people_name, favorite_id= favorite_id)
+    if fav_person is None:
+        fav_person = Favorites(people_name= people_name, favorite_id= favorite_id)
+        db.session.add()
+    else:
+        fav_person = request_body_fav_person[people_name]
     db.session.commit()
 
-    response_body = {
-        "msg": "Personaje agregado correctamente", 
-        "favorite": new_favorite.serialize()
-    }
+    return jsonify(fav_person), 200
 
-    return jsonify(response_body), 200
+# # [DELETE] /favorite/planet/<int:planet_id> Delete favorite planet with the id = planet_id.
 
+@app.route('/favorite/planet/<int:planet_id>', methods=['DELETE'])
+def delete_favorite_planet(planet_name):
 
-#DELETE FAV PLANET
-@app.route('/favorite/planet/<int:favorite_id>', methods=['DELETE'])
-def delete_favorite_planet(favorite_id):
- 
-    favorite = Favorite.query(favorite_id)
-
-    if favorite is None:
-        return jsonify({'msg' : 'No favorite found'}), 404
-    
-    db.session.delete(favorite)
+    fav_planet = Favorites.query.get(planet_name)
+    if fav_planet is None:
+        raise APIException("Favorite planet not found", status_code=404)
+    db.session.delete(fav_planet)
     db.session.commit()
 
-    response_body = {
-        "msg" : "Planeta favorito eliminado correctamente"
-    }
-    return jsonify(response_body), 200
+    return jsonify("Favorite planet deletion successful"), 200
 
-#DELETE FAV CHARACTER
-@app.route('/favorite/character/<int:favorite_id>', methods=['DELETE'])
-def delete_favorite_character(favorite_id):
-    
-    favorite = Favorite.query.get(favorite_id)
+# # [DELETE] /favorite/people/<int:people_id> Delete favorite people with the id = people_id.
 
-    if favorite is None:
-        return jsonify({'msg' : 'No favorite found'}), 404
-    
-    db.session.delete(favorite)
+@app.route('/favorite/people/<int:people_id>', methods=['DELETE'])
+def delete_favorite_people(people_name):
+
+    fav_person = Favorites.query.get(people_name)
+    if fav_person is None:
+        raise APIException("Favorite person not found", status_code=404)
+    db.session.delete(fav_person)
     db.session.commit()
 
-    response_body = {
-        "msg" : "Personaje favorito eliminado correctamente"
-    }
-    return jsonify(response_body), 200
+    return jsonify("Favorite person deletion successful"), 200
+
+@app.route('/favorite/starship/<int:starship_id>', methods=['DELETE'])
+def delete_favorite_starship(starship_name):
+
+    fav_starship = Favorites.query.get(starship_name)
+    if fav_starship is None:
+        raise APIException("Favorite starship not found", status_code=404)
+    db.session.delete(fav_starship)
+    db.session.commit()
+
+    return jsonify("Favorite starship deletion successful"), 200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -4,87 +4,82 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(120), nullable=False)
+    last_name = db.Column(db.String(120), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), nullable=False)
-    favorites = db.relationship('Favorite', backref='users')
+    password = db.Column(db.String(80), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=True, default=True)
 
     def __repr__(self):
-        return '<User %r>' % self.email
+        return '<User %r>' % self.username
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            # do not serialize the password, its a security breach
         }
     
-class Character(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(200), unique=True, nullable=False)
-    description = db.Column(db.String(240))
-    eye_color = db.Column(db.String(100))
-
-    def __repr__(self):
-        return '<Character %r>' % self.name
-
-    def serialize(self):
-        return {
-            "id": self.id,
-            "name": self.name,
-            "description": self.description,
-            "eye_color": self.eye_color
-        }
-    
-class Vehicle(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(200), unique=True, nullable=False)
-    description = db.Column(db.String(240))
-    model = db.Column(db.String(200))
-
-    def __repr__(self):
-        return '<Vehicle %r>' % self.name
-
-    def serialize(self):
-        return {
-            "id": self.id,
-            "name": self.name,
-            "description": self.description,
-            "model": self.model
-        }
-
-class Planet(db.Model):
+class People(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), unique=True, nullable=False)
-    description = db.Column(db.String(240))
-    population = db.Column(db.Integer)
+    type = db.Column(db.String(80), unique=True, nullable=False)
 
     def __repr__(self):
-        return '<Planet %r>' % self.name
+        return '<People %r>' % self.name
 
     def serialize(self):
         return {
             "id": self.id,
             "name": self.name,
-            "description": self.description,
-            "population": self.population
+            "type": self.type,
         }
 
-class Favorite(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    planet_id = db.Column(db.Integer, db.ForeignKey('planet.id'), nullable=True)
-    character_id = db.Column(db.Integer, db.ForeignKey('character.id'), nullable=True)
-
-    planet = db.relationship('Planet')
-    character = db.relationship('Character')
+class Planets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    type = db.Column(db.String(80), unique=True, nullable=False)
 
     def __repr__(self):
-        return f'<Favorite {self.id} >'
+        return '<Planets %r>' % self.name
 
     def serialize(self):
         return {
-            'id': self.id,
-            'user_id': self.user.id,
-            'planet': self.planet.serialize() if self.planet else None,
-            'character': self.character.serialize() if self.character else None
+            "id": self.id,
+            "name": self.name,
+            "type": self.type,
         }
     
+class Starships(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    type = db.Column(db.String(80), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<Starships %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "type": self.type,
+        }
+    
+class Favorites(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("User"))
+    people_name = db.Column(db.String(80), db.ForeignKey("People"))
+    planets_name = db.Column(db.String(80), db.ForeignKey("Planets"))
+    starships_name = db.Column(db.String(80), db.ForeignKey("Starships"))
+
+    def __repr__(self):
+        return '<Favorites %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "people_name": self.people_name,
+            "planets_name": self.planets_name,
+            "starships_name": self.starships_name,
+        }
