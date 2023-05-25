189e500d1036d0c5b9268fb88b4f1ef6d6e5d484
David Avery <avery.di@gmail.com>
2023-05-25 00:05:33 +0000

    minor tweeks

--- src/app.py
+++ src/app.py
@@ -26,19 +26,13 @@ db.init_app(app)
 CORS(app)
 setup_admin(app)
 
-# Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
-# generate sitemap with all your endpoints
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
-
-# # Create an API that connects to a database and implements the following Endpoints (very similar to SWAPI.dev or SWAPI.tech):
-
-# # [GET] /people Get a list of all the people in the database
 
 @app.route('/people', methods=['GET'])
 def handle_people():
@@ -47,8 +41,6 @@ def handle_people():
     all_people = list(map(lambda x: x.serialize(), people))
 
     return jsonify(all_people), 200
-
-# # [GET] /people/<int:people_id> Get a one single people information
 
 @app.route('/people/<int:people_id>', methods=['GET'])
 def handle_person(people_id):
@@ -59,8 +51,6 @@ def handle_person(people_id):
 
     return jsonify(person), 200
 
-# # [GET] /planets Get a list of all the planets in the database
-
 @app.route('/planets', methods=['GET'])
 def handle_planets():
 
@@ -68,8 +58,6 @@ def handle_planets():
     all_planets = list(map(lambda x: x.serialize(), planets))
 
     return jsonify(all_planets), 200
-
-# # [GET] /planets/<int:planet_id> Get one single planet information
 
 @app.route('/planets/<int:planet_id>', methods=['GET'])
 def handle_planet(planet_id):
@@ -80,8 +68,6 @@ def handle_planet(planet_id):
 
     return jsonify(planet), 200
 
-# # [GET] /starships Get a list of all the starships in the database
-
 @app.route('/starships', methods=['GET'])
 def handle_starships():
 
@@ -89,8 +75,6 @@ def handle_starships():
     all_starships = list(map(lambda x: x.serialize(), starships))
 
     return jsonify(all_starships), 200
-
-# # [GET] /starships/<int:starship_id> Get one single starship information
 
 @app.route('/starships/<int:starship_id>', methods=['GET'])
 def handle_starship(starship_id):
@@ -100,10 +84,6 @@ def handle_starship(starship_id):
         raise APIException("User not found", status_code=404)
 
     return jsonify(starship), 200
-
-# # Additionally create the following endpoints to allow your StartWars blog to have users and favorites:
-
-# # [GET] /users Get a list of all the blog post users
 
 @app.route('/users', methods=['GET'])
 def handle_users():
@@ -156,8 +136,6 @@ def delete_user(user_id):
 
     return jsonify("User deletion successful"), 200
 
-# # [GET] /users/favorites Get all the favorites that belong to the current user.
-
 @app.route('/users/favorites', methods=['GET'])
 def handle_favorites():
 
@@ -165,8 +143,6 @@ def handle_favorites():
     all_favorites = list(map(lambda x: x.serialize(), favorites))
 
     return jsonify(all_favorites), 200
-
-# # [POST] /favorite/starship/<int:starship_id> Add a new favorite starship to the current user with the starship id = starship_id.
 
 @app.route('/favorite/starship/<int:starship_id>', methods=['POST'])
 def create_favorite_starship(starship_name):
@@ -185,8 +161,6 @@ def create_favorite_starship(starship_name):
 
     return jsonify(fav_star), 200
 
-# # [POST] /favorite/planet/<int:planet_id> Add a new favorite planet to the current user with the planet id = planet_id.
-
 @app.route('/favorite/planet/<int:planet_id>', methods=['POST'])
 def create_favorite_planet(planet_name):
 
@@ -203,8 +177,6 @@ def create_favorite_planet(planet_name):
     db.session.commit()
 
     return jsonify(fav_planet), 200
-
-# # [POST] /favorite/people/<int:people_id> Add new favorite people to the current user with the people id = people_id.
 
 @app.route('/favorite/people/<int:people_id>', methods=['POST'])
 def create_favorite_people(people_name):
@@ -223,8 +195,6 @@ def create_favorite_people(people_name):
 
     return jsonify(fav_person), 200
 
-# # [DELETE] /favorite/planet/<int:planet_id> Delete favorite planet with the id = planet_id.
-
 @app.route('/favorite/planet/<int:planet_id>', methods=['DELETE'])
 def delete_favorite_planet(planet_name):
 
@@ -235,8 +205,6 @@ def delete_favorite_planet(planet_name):
     db.session.commit()
 
     return jsonify("Favorite planet deletion successful"), 200
-
-# # [DELETE] /favorite/people/<int:people_id> Delete favorite people with the id = people_id.
 
 @app.route('/favorite/people/<int:people_id>', methods=['DELETE'])
 def delete_favorite_people(people_name):
@@ -260,7 +228,6 @@ def delete_favorite_starship(starship_name):
 
     return jsonify("Favorite starship deletion successful"), 200
 
-# this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=False)
